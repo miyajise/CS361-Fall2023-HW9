ca22ebc84f51fce2b7df99b796c86af55d38424a
miyajise <91396602+miyajise@users.noreply.github.com>
2023-11-20 19:02:50 -0800

    Files Added.

---
+++ PlayerAccount.py
@@ -0,0 +1,65 @@
+# Name: Sharyn Miyaji
+# Date: 11/20/2023
+# Course: CS361 Section 400
+
+
+import time
+
+class PlayerAccount:
+
+    def __init__(self):
+        self._playerlist = {}
+
+
+    def withdraw_money(self, name, amount):
+        current_amount = self._playerlist[name]["Amount"]
+        if amount >= current_amount:
+            current_amount -= amount
+            self._playerlist[name]["Amount"] = current_amount
+        else:
+            print("Insufficient amount")
+
+    def deposit_money(self, name, amount):
+        if name not in self._playerlist:
+            self._playerlist[name] = {"Name": name,
+                                      "Amount": 0}
+        current_amount = self._playerlist[name]["Amount"]
+        current_amount += amount
+        self._playerlist[name]["Amount"] = current_amount
+
+    def get_amount(self, name):
+        current_amount = self._playerlist[name]["Amount"]
+        return current_amount
+
+
+account = PlayerAccount()
+while True:
+    input_file = open('bankcomms.txt', 'r')
+    first_line = input_file.readline()
+    second_line = input_file.readline()
+    second_line_split = second_line.split(' ')
+    if "request" in first_line:
+
+        time.sleep(2)
+        name = str(second_line_split[0])
+        action = str(second_line_split[1])
+        amount = float(second_line_split[2])
+
+        if action == "deposit":
+            account.deposit_money(name, amount)
+            action_taken = "Money deposited."
+            total_amount = account.get_amount(name)
+        elif action == "retrieve":
+            account.withdraw_money(name, amount)
+            action_taken = "Money withdrawn."
+            total_amount = account.get_amount(name)
+        time.sleep(2)
+        output_file = open('bankcomms.txt', 'w')
+        output_file.write("Successfully received")
+        output_file.close()
+
+    time.sleep(10)
+
+
+
+
---
+++ README.md
@@ -0,0 +1,20 @@
+#CS361 Assignment 9 
+by Sharyn Miyaji
+
+Partner: James Hill
+
+
+**How to programmatically REQUEST data:**
+
+In the bankcomms.txt file, the first line must contain the word "request" in order to start the microservice.  After
+the first line, the name of the player, the action ('deposit'/'receive'), and the amount must be written using a space to
+separate the variables.  
+
+**How to programmatically RECEIVE data:**
+
+Once the request is completed, the bankcomms.txt file will have the phrase "Successfully received" to indicate that the
+transaction is completed for the player.  
+
+**UML Diagram:**
+
+![](UML_diagram.png)
